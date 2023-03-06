0cef507bba37e54cac2529f50f2bc68852e2c9c2
ueb14603 <61944226+gamma19@users.noreply.github.com>
2023-03-06 18:09:30 +0300

    up3
    up3

--- client/src/components/pages/women/Women.js
+++ client/src/components/pages/women/Women.js
@@ -1,50 +1,93 @@
 import React from 'react'
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './Women.css'
 
 function Women() {
+  
   return (
     <>
-    <Box className="women-filter" bgcolor='white' flex={1}>
-      <div className='filter-inner-box'>
-
-      </div>
-    </Box>
-    <Box className="women-product-container" bgcolor='white' flex={2}>
-      <div class="container page-wrapper">
-      <div class="page-inner">
-      <div class="row">
-        <div class="el-wrapper">
-        <div class="box-up">
-          <img class="img" src="http://code.slicecrowd.com/labs/4/images/t-shirt.png" alt=""/>
-          <div class="img-info">
-            <div class="info-inner">
-              <span class="p-name">I feel like Pablo</span>
-              <span class="p-company">Yeezy</span>
-            </div>
-            <div class="a-size">Available sizes :: <span class="size">S , M , L , XL</span></div>
-          </div>
-        </div>
-
-        <div class="box-down">
-          <div class="h-bg">
-            <div class="h-bg-inner"></div>
-          </div>
-
-          <a class="cart" href="#">
-            <span class="price">$120</span>
-            <span class="add-to-cart">
-              <span class="txt">Add in cart</span>
-            </span>
-          </a>
-        </div>
-      </div>
-
+      <Box className="women-filter" bgcolor='white' flex={1}>
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
+      <Box className="women-product-container" bgcolor='white' flex={2}>
+      <Box sx={{ flexGrow: 1, p: 2 }}>
       
-    </div>
-  </div>
-</div>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          '--Grid-borderWidth': '1px',
+          borderTop: 'var(--Grid-borderWidth) solid',
+          borderLeft: 'var(--Grid-borderWidth) solid',
+          borderColor: 'divider',
+          '& > div': {
+            borderRight: 'var(--Grid-borderWidth) solid',
+            borderBottom: 'var(--Grid-borderWidth) solid',
+            borderColor: 'divider',
+          },
+        }}
+      >
+        {[...Array(12)].map((_, index) => (
+          <Grid key={index} {...{ xs: 12, sm: 6, md: 4, lg: 3 }} minHeight={240} />
+        ))}
+
+        
+      </Grid>
     </Box>
+
+      </Box>
+
+    
     </>
   )
 }
